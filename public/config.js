4298f53666bd922211c91ef99a00e87560eecc0e
fredsiika <fredsiika@gmail.com>
2018-05-28 06:52:06 -0500

    Create and export configuration variables.

---
+++ public/config.js
@@ -0,0 +1,27 @@
+/*
+* Create and export configuration variable
+*
+*/
+
+// Container  for all environments
+var environments = {};
+
+// Staging (default) environment
+environment.staging = {
+    'httpPort' : 3000,
+    'httpsPort' : 3001
+    'envName' : 'staging'
+};
+
+ // Production environment
+ environment.production = {
+     'httpPort' : 5000,
+     'httpsPort' : 5001,
+     'envName' : 'production'
+ };
+
+ // Determine which environment was passed as a command-line argument
+ var currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+
+// Export the module
+module.exports = environmentToExport;
